93619f3938939aa8d53e5d270002db4c1e3c21e3
YUDA <yudajgim@gmail.com>
2023-01-16 15:23:29 +0900

    git init

---
+++ src/main/java/com/msa/example/chapter01/MsaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.msa.example.chapter01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaProjectApplication.class, args);
+    }
+
+}
