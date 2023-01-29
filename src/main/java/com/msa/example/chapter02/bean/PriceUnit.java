033b8b199591b1fac2cd6e753b357c6bf6a28d19
YUDA <yudajgim@gmail.com>
2023-01-29 15:34:28 +0900

    스테레오 타입 어노테이션을 이용해 스프링 빈 생성

--- src/main/java/com/msa/example/chapter02/bean/PriceUnit.java
+++ src/main/java/com/msa/example/chapter02/bean/PriceUnit.java
@@ -1,4 +1,4 @@
-package com.msa.example.chapter02;
+package com.msa.example.chapter02.bean;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/com/msa/example/chapter02/bean/SpringBeanApplication.java
+++ src/main/java/com/msa/example/chapter02/bean/SpringBeanApplication.java
@@ -1,4 +1,4 @@
-package com.msa.example.chapter02;
+package com.msa.example.chapter02.bean;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
