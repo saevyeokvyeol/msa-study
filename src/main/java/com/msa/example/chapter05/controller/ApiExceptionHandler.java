6a52f1db181135842b58d0114815e7e14f2c5491
YUDA <yudajgim@gmail.com>
2023-02-13 14:15:33 +0900

    5장 [스프링 MVC를 이용한 REST-API 개발] 실습 (4) - 미디어 컨텐츠 다운로드

--- src/main/java/com/msa/example/chapter05/controller/ApiExceptionHandler.java
+++ src/main/java/com/msa/example/chapter05/controller/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.msa.example.chapter05.controller;
 
 import com.msa.example.chapter05.domain.BadRequestException;
+import com.msa.example.chapter05.domain.FileDownloadException;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,5 +27,12 @@ public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handlerException(Exception ex) {
         return new ResponseEntity<>(new ErrorResponse("system error"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(FileDownloadException.class)
+    public ResponseEntity handleFileDownloadException(FileDownloadException ex) {
+        // PDF를 내려받는 API에서 발생하는 에러이므로 별도로 표현할 메시지 X
+        // HTTP 상태 코드만 응답
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/msa/example/chapter05/controller/ReservationController.java
+++ src/main/java/com/msa/example/chapter05/controller/ReservationController.java
@@ -1,10 +1,19 @@
 package com.msa.example.chapter05.controller;
 
+import com.msa.example.chapter05.domain.FileDownloadException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,5 +41,56 @@ public class ReservationController {
         });
 
         return Collections.emptyList();
+    }
+
+    /*
+     * HttpMessageConverter 객체를 사용해 bytes[]로 클라이언트에 파일을 전달
+     * */
+    @GetMapping("/hotels/{hotelId}/rooms/{roomNumber}/reservations/{reservationId}")
+    public ResponseEntity<byte[]> getInvoice(@PathVariable Long hotelId,
+                                             @PathVariable String roomNumber,
+                                             @PathVariable Long reservationId) {
+        // 해당 프로젝트의 resources/pdf/hotel_invoice.pdf
+        String filePath = "pdf/hotel_invoice.pdf";
+
+        // 파일 경로 변수 filePath에서 InputStream을 가져옴
+        try (InputStream inputStream = new ClassPathResource(filePath).getInputStream()) {
+            // inputStream 객체를 byte[] bytes으로 변환
+            byte[] bytes = StreamUtils.copyToByteArray(inputStream);
+
+            // 변환한 bytes와 200 OK 상태 코드로 ResponseEntity 객체를 생성해 리턴
+            return new ResponseEntity<>(bytes, HttpStatus.OK);
+        } catch (Throwable th) {
+            th.printStackTrace();
+            throw new FileDownloadException("file download error");
+        }
+    }
+
+    /*
+    * HttpServletResponse 객체를 사용해 클라이언트에 파일을 전달
+    * */
+    @GetMapping(value = "/hotels/{hotelId}/rooms/{roomNumber}/reservations/{reservationId}", produces = "application/pdf")
+    public void downloadInvoice(@PathVariable Long hotelId,
+                                @PathVariable String roomNumber,
+                                @PathVariable Long reservationId,
+                                // 런타임 과정에서 생성된 HttpServletResponse 객체 주입
+                                HttpServletResponse response) {
+        String filePath = "pdf/hotel_invoice.pdf";
+
+        try (InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+             OutputStream outputStream = response.getOutputStream()) {
+
+            // HttpServletResponse 객체의 메소드를 사용해 HTTP 상태 코드와 헤더 설정
+            // HttpServletResponse는 Content-type 헤더를 설정하는 setContentType 메소드 제공
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader("Content-Disposition", "filename=hotel_invoice.pdf");
+
+            // StreamUtils.copy() 메소드를 이용해 inputStream에서 데이터를 읽고 outputStream으로 데이터를 쓺
+            StreamUtils.copy(inputStream, outputStream);
+        } catch (Throwable th) {
+            th.printStackTrace();
+            throw new FileDownloadException("file download error");
+        }
     }
 }
